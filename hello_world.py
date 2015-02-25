710a6f43b44d453b66d14adf4bfcfa5cdc312f2d
cavens <jef@cavens.io>
2015-02-25 18:39:50 +0000

    Engine done

--- hello_world.py
+++ hello_world.py
@@ -1,43 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def template_test():
+  return render_template('template.html', my_string = "Whieeeeeee", my_list = [0,1,2,3,4,5,6,7,8,9],current_time = datetime.datetime.now()
+)
 
 
 @app.route("/hello")
 def hello_world():
   return "Hello World!"
 
-
 @app.route("/hello/<name>")
 def hello_person(name):
-  html = """
-  <h1>
-    Hello {}!
-  </h1>
-  <p>
-    Here's a picture of a kitten. Wiejoooo....
-  </p>
-  <img src = "http://placekitten.com/g/200/300">  
-  """
-  return html.format(name.title())
+  return render_template('hello.html', user_name = name.title())
 
 
 @app.route("/jedi/<first>/<last>")
 def jedi_name(first,last):
-  html ="""
-  <h1>
-    Hi {}!
-  </h1>
-  <p>
-    You're a true Jedi now...
-  </p>
-  <img src = "http://media.giphy.com/media/N8Lfh9gWcWYIU/giphy.gif">  
-  """
   last_part = last[:3]
   first_part = first[:2]
   jedi_name = last_part + first_part
-  return html.format(jedi_name)
+  return render_template('jedi.html', jedi_name_html = jedi_name)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
